688b87e197a18c1a57d48c385d99da920afd1b9d
AlexandreAlmaraz <alexalmarazm14@gmail.com>
2024-11-15 21:33:47 -0600

    first commit

---
+++ src/main/java/com/upiiz/memoria/controllers/ControllerFacturas.java
@@ -0,0 +1,25 @@
+package com.upiiz.memoria.controllers;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v2")
+public class ControllerFacturas {
+    @GetMapping("/listar")
+    public String listar(){
+        return "lista de facturas - sin seguridad";
+    }
+
+    @GetMapping("/actualizar")
+    public String actualizar(){
+        return "Factura acualizada - con seguridad";
+    }
+
+    @GetMapping("/eliminar")
+    public String eliminar(){
+        return "Factura eliminada - con seguridad";
+    }
+}
